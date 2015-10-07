d58efbaed03d810b8299c8a608f2a62ff9abe193
carmenmokz <carmenmok1697@gmail.com>
2015-10-06 19:23:35 -0600

    Leer txt
    Prueba de método que lee archivos txt

--- Algoritmo Genético/include/Problema.h
+++ Algoritmo Genético/include/Problema.h
@@ -1,6 +1,10 @@
 #ifndef PROBLEMA_H
 #define PROBLEMA_H
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
 
 class Problema
 {
@@ -13,6 +17,25 @@ class Problema
         virtual int geneSize()=0;
         virtual double fitness()=0;
         virtual void name()=0;
+};
+
+class VertexCover: public Problema
+{
+    public:
+        void readProblema(){
+            ifstream enarchivo;
+            enarchivo.open("cromosoma.txt");
+            int a,b,c,d;
+            enarchivo >> a >> b >> c >> d;
+
+            //ERROR SI EL ARCHIVO NO ESTÁ CREADO
+            if (enarchivo.fail()){
+                cerr<<"el archivo no fue encontrado \n";
+                exit(1);
+            }
+            cout << a << " " << b << " " << c << " " << d; //lee cada linea del txt
+            return 0;
+        }
 };
 
 #endif // PROBLEMA_H
--- Algoritmo Genético/main.cpp
+++ Algoritmo Genético/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    ifstream enarchivo;
+            enarchivo.open("cromosoma.txt");
+            int a,b,c,d;
+            enarchivo >> a >> b >> c >> d;
+
+            //ERROR SI EL ARCHIVO NO ESTÁ CREADO
+            if (enarchivo.fail()){
+                cerr<<"el archivo no fue encontrado \n";
+                exit(1);
+            }
+            cout << a << " " << b << " " << c << " " << d;
+            return 0;
 }
